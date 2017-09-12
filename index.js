9c649ce28b521f7bbea2d9dae4747f688f97bd6c
Michiel de Jong <michiel@ripple.com>
2017-09-12 21:34:29 +0200

    payment fixes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const ClpNode = require('../clp-node')
 const ClpPacket = require('clp-packet')
 const IlpPacket = require('ilp-packet')
+const crypto = require('crypto')
+
+function generateRequestId() {
+ const buf = crypto.randomBytes(4)
+ const hex = buf.toString('hex')
+ return parseInt(hex, 16)
+}
 
 function MakeProtocolData(obj) {
   let protocolData = [
@@ -44,206 +51,275 @@ function Frog (config) {
 Frog.prototype = {
   registerPluginEventHandlers() {
     this.plugin.on('incoming_prepare', (transfer) => {
-      this.ws.send(ClpPacket.serialize({
-        type: Clp.TYPE_PREPARE,
-        requestId: uuid(),
-        data: {
-          from: transfer.from,
-          expiresAt: new Date(transfer.expiresAt),
-          amount: parseInt(transfer.amount),
-          executionCondition: Buffer.from(transfer.executionCondition, 'base64'),
-          protocolData: MakeProtocolData(transfer)
-        }
-      }))
+      console.log('incoming prepare!', transfer)
+      try {
+        this.ws.send(ClpPacket.serialize({
+          type: ClpPacket.TYPE_PREPARE,
+          requestId: generateRequestId(),
+          data: {
+            transferId: transfer.id,
+            expiresAt: new Date(transfer.expiresAt),
+            amount: parseInt(transfer.amount),
+            executionCondition: Buffer.from(transfer.executionCondition, 'base64'),
+            protocolData: MakeProtocolData(transfer)
+          }
+        }))
+      } catch(e) {
+        console.error(e)
+      }
     })
     this.plugin.registerRequestHandler((request) => { 
       const promise = new Promise((resolve, reject) => {
         this.requestsReceived[request.id] = { resolve, reject }
       })
       this.ws.send(ClpPacket.serialize({
-        type: Clp.TYPE_MESSAGE,
+        type: ClpPacket.TYPE_MESSAGE,
         requestId: request.id,
         data: MakeProtocolData(request)
       }))
       return promise
     })
     this.plugin.on('outgoing_fulfill', (transfer, fulfillment) => {
-      this.ws.send(ClpPacket.serialize({
-        type: Clp.TYPE_FULFILL,
-        requestId: uuid(),
-        data: {
-          transferId: transfer.id,
-          fulfillment: Buffer.from(fulfillment, 'base64') 
-        }
-      }))
+      console.log('our prepare was fulfilled on-ledger!', transfer, fulfillment)
+      try {
+        this.ws.send(ClpPacket.serialize({
+          type: ClpPacket.TYPE_FULFILL,
+          requestId: generateRequestId(),
+          data: {
+            transferId: transfer.id,
+            fulfillment: Buffer.from(fulfillment, 'base64'),
+            protocolData: []
+          }
+        }))
+      } catch(e) {
+        console.error(e)
+      }
     })
     this.plugin.on('outgoing_reject', (transfer, rejectionReason) => {
+      try {
+        this.ws.send(ClpPacket.serialize({
+          type: ClpPacket.TYPE_REJECT,
+          requestId: generateRequestId(),
+          data: {
+            transferId: transfer.id,
+            rejectionReason,
+            protocolData: []
+          }
+        }))
+      } catch(e) {
+        console.error(e)
+      }
+    })
+  },
+
+  _handleIlpMessage(obj) {
+    const lpiRequest = {
+      id: obj.requestId.toString(),
+      from: this.plugin.getAccount(),
+      to: protocolDataAsObj.to.data.toString('ascii'),
+      ledger: this.plugin.getInfo().prefix,
+      ilp: protocolDataAsObj.ilp.data.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''),
+      custom: {}
+    }
+    this.plugin.sendRequest(lpiRequest).then((response) => {
+      const responsePacketBuf = Buffer.from(response.ilp, 'base64')
+      const ilpResponse = IlpPacket.deserializeIlpPacket(responsePacketBuf)
+      const responseProtocolData = MakeProtocolData(response)
+      if (ilpResponse.type ===  IlpPacket.TYPE_ILP_ERROR) {
+        this.ws.send(ClpPacket.serialize({
+          type: ClpPacket.TYPE_ERROR,
+          requestId: obj.requestId,
+          data: {
+            rejectionReason: err,
+            protocolData: responseProtocolData
+          }
+        }))
+      } else {
+        this.ws.send(ClpPacket.serialize({
+          type: ClpPacket.TYPE_RESPONSE,
+          requestId: obj.requestId,
+          data: MakeProtocolData(response)
+        }))
+      }
+    }, err => {
       this.ws.send(ClpPacket.serialize({
-        type: Clp.TYPE_REJECT,
-        requestId: uuid(),
+        type: ClpPacket.TYPE_ERROR,
+        requestId: obj.requestId,
         data: {
-          transferId: transfer.id,
-          rejectionReason
+          rejectionReason: err,
+          protocolData: []
         }
       }))
-      this.clp.sendCall(Clp.TYPE_REJECT, {
-        transferId: transfer.id,
-      })
     })
+  },
+
+  _handleInfoMessage(obj) {
+    if (obj.data[0].data[0] === 0) {
+      this.ws.send(ClpPacket.serialize({
+        type: ClpPacket.TYPE_RESPONSE,
+        requestId: obj.requestId,
+        data: [
+          {
+            protocolName: 'info',
+            contentType: ClpPacket.MIME_TEXT_PLAIN_UTF8,
+            data: Buffer.from(this.plugin.getAccount(), 'ascii')
+          }
+        ]
+      }))
+    } else {
+      this.ws.send(ClpPacket.serialize({
+        type: ClpPacket.TYPE_RESPONSE,
+        requestId: obj.requestId,
+        data: [
+          {
+            protocolName: 'info',
+            contentType: ClpPacket.MIME_APPLICATION_JSON,
+            data: Buffer.from(JSON.stringify(this.plugin.getInfo()), 'ascii')
+          }
+        ]
+      }))
+    }
+  },
+
+  _handleBalanceMessage(obj) {
+    this.plugin.getBalance().then(decStr => {
+      let hexStr = parseInt(decStr).toString(16)
+      if (hexStr.length % 2 === 1) {
+        hexStr = '0' + hexStr
+      }
+      let balanceBuf = Buffer.from(hexStr, 'hex')
+      while (balanceBuf.length < 8) {
+        balanceBuf = Buffer.concat([ Buffer.from([ 0 ]), balanceBuf ])
+      }
+      this.ws.send(ClpPacket.serialize({
+        type: ClpPacket.TYPE_RESPONSE,
+        requestId: obj.requestId,
+        data: [
+          {
+            protocolName: 'balance',
+            contentType: ClpPacket.MIME_APPLICATION_OCTET_STREAM,
+            data: balanceBuf
+          }
+        ]
+      }))
+    })
+  },
+
+  _handleMessage(obj) {
+    switch (obj.data[0].protocolName) {
+      case 'ilp':
+        return this._handleIlpMessage(obj)
+        break
+      case 'info':
+        return this._handleInfoMessage(obj)
+        break
+      case 'balance':
+        return this._handleBalanceMessage(obj)
+        break
+    }
   },
 
   registerWebSocketMessageHandler() {
     this.ws.on('message', (buf) => {
-      const obj = ClpPacket.deserialize(buf)
-      let protocolDataAsObj = {}
-      let protocolDataAsArr
-      if ([ClpPacket.TYPE_ACK, ClpPacket.TYPE_RESPONSE, ClpPacket.TYPE_MESSAGE].indexOf(obj.type) !== -1) {
-        protocolDataAsArr = obj.data
-      } else {
-        protocolDataAsArr = obj.data.protocolData
-      }
-      for (let i = 0; i < protocolDataAsArr.length; i++) {
-        protocolDataAsObj[protocolDataAsArr[i].protocolName] = protocolDataAsArr[i]
-      }
-      switch (obj.type) {
-        case ClpPacket.TYPE_ACK:
-          this.requestsReceived[obj.requestId].resolve()
-          delete this.requestsSent[obj.requestId]
-          break
-        case ClpPacket.TYPE_RESPONSE:
-          if (Array.isArray(obj.data) && obj.data.length) {
-            this.requestsReceived[obj.requestId].resolve(obj.data[0])
-          } else { // treat it as an ACK, see https://github.com/interledger/rfcs/issues/283
-            this.requestsReceived[obj.requestId].resolve()
-          }
-          delete this.requestsSent[obj.requestId]
-          break
-
-        case ClpPacket.TYPE_ERROR:
-          // according to LPI, an error response should fulfill (not reject) the request handler promise
-          this.requestsReceived[obj.requestId].fulfill(obj.data.rejectionReason)
-          delete this.requestsSent[obj.requestId]
-          break
-
-        case ClpPacket.TYPE_PREPARE:
-          this.plugin.sendTransfer({
-            id: obj.data.transferId.toString(),
-            from: this.plugin.getAccount(),
-            to: protocolDataAsObj.to.data,
-            ledger: this.plugin.getInfo().prefix,
-            amount: obj.data.amount.toString(),
-            ilp: protocolDataAsObj.ilp.data.toString('base64'),
-            noteToSelf: {},
-            executionCondition: transfer.executionCondition.toString('base64'),
-            expiresAt: transfer.expiresAt.toISOString(),
-            custom: {}
-          })
-          break
-
-        case ClpPacket.TYPE_FULFILL:
-          this.plugin.fulfillCondition(obj.data.transferId, obj.data.fulfillment.toString('base64'))
-          break
-
-        case ClpPacket.TYPE_REJECT:
-          this.plugin.rejectIncomingTransfer(obj.data.transferId, IlpPacket.deserializeIlpError(obj.data.rejectionReason))
-          break
-
-        case ClpPacket.TYPE_MESSAGE:
-          switch (obj.data[0].protocolName) {
-            case 'ilp':
-              const lpiRequest = {
-                id: obj.requestId.toString(),
-                from: this.plugin.getAccount(),
-                to: protocolDataAsObj.to.data.toString('ascii'),
-                ledger: this.plugin.getInfo().prefix,
-                ilp: protocolDataAsObj.ilp.data.toString('base64'),
-                custom: {}
+      try {
+        const obj = ClpPacket.deserialize(buf)
+        console.log('message reached frog over CLP WebSocket!', obj)
+        let protocolDataAsObj = {}
+        let protocolDataAsArr
+        if ([ClpPacket.TYPE_ACK, ClpPacket.TYPE_RESPONSE, ClpPacket.TYPE_MESSAGE].indexOf(obj.type) !== -1) {
+          protocolDataAsArr = obj.data
+        } else {
+          protocolDataAsArr = obj.data.protocolData
+        }
+        for (let i = 0; i < protocolDataAsArr.length; i++) {
+          protocolDataAsObj[protocolDataAsArr[i].protocolName] = protocolDataAsArr[i]
+        }
+        switch (obj.type) {
+          case ClpPacket.TYPE_ACK:
+            // If it's a response to sendRequest, then resolve it:
+            if (this.requestsReceived[obj.requestId]) {
+              this.requestsReceived[obj.requestId].resolve()
+              delete this.requestsSent[obj.requestId]
+            }
+            break
+          case ClpPacket.TYPE_RESPONSE:
+            // If it's a response to sendRequest, then resolve it:
+            if (this.requestsReceived[obj.requestId]) {
+              if (Array.isArray(obj.data) && obj.data.length) {
+                this.requestsReceived[obj.requestId].resolve(obj.data[0])
+              } else { // treat it as an ACK, see https://github.com/interledger/rfcs/issues/283
+                this.requestsReceived[obj.requestId].resolve()
               }
-              this.plugin.sendRequest(lpiRequest).then((response) => {
-                const responsePacketBuf = Buffer.from(response.ilp, 'base64')
-                const ilpResponse = IlpPacket.deserializeIlpPacket(responsePacketBuf)
-                const responseProtocolData = MakeProtocolData(response)
-                if (ilpResponse.type ===  IlpPacket.TYPE_ILP_ERROR) {
-                  this.ws.send(ClpPacket.serialize({
-                    type: ClpPacket.TYPE_ERROR,
-                    requestId: obj.requestId,
-                    data: {
-                      rejectionReason: err,
-                      protocolData: responseProtocolData
-                    }
-                  }))
-                } else {
-                  this.ws.send(ClpPacket.serialize({
-                    type: ClpPacket.TYPE_RESPONSE,
-                    requestId: obj.requestId,
-                    data: MakeProtocolData(response)
-                  }))
-                }
+              delete this.requestsSent[obj.requestId]
+            }
+            break
+
+          case ClpPacket.TYPE_ERROR:
+            // If it's a response to sendRequest, then resolve it:
+            if (this.requestsReceived[obj.requestId]) {
+              // according to LPI, an error response should fulfill (not reject) the request handler promise
+              this.requestsReceived[obj.requestId].fulfill(obj.data.rejectionReason)
+              delete this.requestsSent[obj.requestId]
+            }
+            break
+
+          case ClpPacket.TYPE_PREPARE:
+            const lpiTransfer = {
+              id: obj.data.transferId.toString(),
+              from: this.plugin.getAccount(),
+              to: protocolDataAsObj.to.data.toString('ascii'),
+              ledger: this.plugin.getInfo().prefix,
+              amount: obj.data.amount.toString(),
+              ilp: protocolDataAsObj.ilp.data.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''),
+              noteToSelf: {},
+              executionCondition: obj.data.executionCondition.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''),
+              expiresAt: obj.data.expiresAt.toISOString(),
+              custom: {}
+            }
+            console.log('preparing on-ledger', lpiTransfer)
+            try {
+              this.plugin.sendTransfer(lpiTransfer).then(result => {
+                console.log('prepared', result)
               }, err => {
-                this.ws.send(ClpPacket.serialize({
-                  type: ClpPacket.TYPE_ERROR,
-                  requestId: obj.requestId,
-                  data: {
-                    rejectionReason: err,
-                    protocolData: []
-                  }
-                }))
+                console.error(err)
               })
-              break
-            case 'info':
-              if (obj.data[0].data[0] === 0) {
-                this.ws.send(ClpPacket.serialize({
-                  type: ClpPacket.TYPE_RESPONSE,
-                  requestId: obj.requestId,
-                  data: [
-                    {
-                      protocolName: 'info',
-                      contentType: ClpPacket.MIME_TEXT_PLAIN_UTF8,
-                      data: Buffer.from(this.plugin.getAccount(), 'ascii')
-                    }
-                  ]
-                }))
-              } else {
-                this.ws.send(ClpPacket.serialize({
-                  type: ClpPacket.TYPE_RESPONSE,
-                  requestId: obj.requestId,
-                  data: [
-                    {
-                      protocolName: 'info',
-                      contentType: ClpPacket.MIME_APPLICATION_JSON,
-                      data: Buffer.from(JSON.stringify(this.plugin.getInfo()), 'ascii')
-                    }
-                  ]
-                }))
-              }
-              break
-            case 'balance':
-              this.plugin.getBalance().then(decStr => {
-                let hexStr = parseInt(decStr).toString(16)
-                if (hexStr.length % 2 === 1) {
-                  hexStr = '0' + hexStr
+            } catch(e) {
+              console.error(e)
+            }
+            break
+
+          case ClpPacket.TYPE_FULFILL:
+            const fulfillmentBase64 = obj.data.fulfillment.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+            this.plugin.fulfillCondition(obj.data.transferId, fulfillmentBase64).then(() => {
+              this.ws.send(ClpPacket.serialize({
+                type: ClpPacket.TYPE_ACK,
+                requestId: obj.requestId,
+                data: []
+              }))
+            }, err => {
+              this.ws.send(ClpPacket.serialize({
+                type: ClpPacket.TYPE_ERROR,
+                requestId: obj.requestId,
+                data: {
+                  rejectionReason: Buffer.from(err.message, 'ascii'), // TODO: use the right error object here ...
+                  protocolData: []
                 }
-                let balanceBuf = Buffer.from(hexStr, 'hex')
-                while (balanceBuf.length < 8) {
-                  balanceBuf = Buffer.concat([ Buffer.from([ 0 ]), balanceBuf ])
-                }
-                this.ws.send(ClpPacket.serialize({
-                  type: ClpPacket.TYPE_RESPONSE,
-                  requestId: obj.requestId,
-                  data: [
-                    {
-                      protocolName: 'balance',
-                      contentType: ClpPacket.MIME_APPLICATION_OCTET_STREAM,
-                      data: balanceBuf
-                    }
-                  ]
-                }))
-              })
-              break
-          }
-          break
-        default:
-         // ignore
+              }))
+            })
+            break
+
+          case ClpPacket.TYPE_REJECT:
+            this.plugin.rejectIncomingTransfer(obj.data.transferId, IlpPacket.deserializeIlpError(obj.data.rejectionReason))
+            break
+
+          case ClpPacket.TYPE_MESSAGE:
+            this.handleMessage(obj)
+            break
+          default:
+           // ignore
+        }
+      } catch(e) {
+        console.error(e)
       }
     })
   },
